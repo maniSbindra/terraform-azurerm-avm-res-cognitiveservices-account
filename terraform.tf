3b990c4fbf5b36f85407fb5d5c400718f033daf5
Mani Bindra <maniSbindra@users.noreply.github.com>
2024-12-02 17:16:29 +0530

    use azurerm version 4

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ terraform {
     # TODO: Ensure all required providers are listed here.
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0, < 4.0"
+      version = ">= 3.71.0, < 5.0"
     }
     modtm = {
       source  = "Azure/modtm"
